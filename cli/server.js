06e3c8cd314d0e097f39a4be0fbe798f3f783b0f
Tom Scott <tscott@weblinc.com>
2020-04-20 15:25:22 -0400

    Reduce Security Footprint of `saur server`
    The Saur CLI doesn't need to have `--allow-net` access to do its job,
    only the ability to read and write from the filesystem. Previously, this
    command worked by importing `config/server.js` and allowing it to run,
    but this required the entire CLI having network access, which throws
    away Deno's security model. Now that the CLI actually forks a process to
    the `bin/server` command located within the application directly,
    instead of importing the application code on its own, it no longer needs
    `--allow-net` access and can just be responsible for starting
    `bin/server` in an application directory. The `bin/server` command has a
    different set of privileges than `saur`, allowing read/write only to the
    current application directory, as well as full network access.

--- cli/server.js
+++ cli/server.js
@@ -1,3 +1,21 @@
+const { run, cwd } = Deno;
+
 export default async function Server() {
-  await import(`${Deno.cwd()}/config/server.js`);
+  const cmd = [`${cwd()}/bin/server`];
+  const stdout = "piped";
+  const stderr = "piped";
+  const decoder = new TextDecoder();
+  const proc = run({ cmd, stdout, stderr });
+  const buff = new Uint8Array(80);
+  let done = false;
+
+  while (!done) {
+    await proc.stdout.read(buff);
+    console.log(decoder.decode(buff));
+    done = buff.toString() === "";
+  }
+
+  const status = await proc.status();
+
+  Deno.exit(status);
 }
