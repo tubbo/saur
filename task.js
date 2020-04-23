06126892e1001d427d7ea104984ef14fd87c846c
Tom Scott <tscott@weblinc.com>
2020-04-22 23:37:58 -0400

    Add more info about the task() function

--- task.js
+++ task.js
@@ -2,7 +2,10 @@ import { walkSync } from "https://deno.land/std/fs/mod.ts";
 
 /**
  * Tasks are user-defined subcommands of the `saur` CLI that are active
- * when the user is at the top level of an application directory.
+ * when the user is at the top level of an application directory. They
+ * are typically created using the `task()` function which is the
+ * default export of this module. Generally, `Task` classes do not need
+ * to be instantiated or used direcrlty.
  */
 export class Task {
   static async all() {
@@ -37,6 +40,10 @@ export class Task {
   }
 }
 
+/**
+ * Create a new task with the given `name` and `description`, calling
+ * the function when the task is invoked.
+ */
 export default function task(name, description, perform) {
   return new Task({ name, description, perform });
 }
